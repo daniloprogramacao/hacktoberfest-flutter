a2aa78167676cc9d627d3229e49d06a2a298339a
Cristian Marques <cristianmarques97@hotmail.com>
2021-10-31 10:21:03 -0300

    Class refractor

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// main class
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
